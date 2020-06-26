bf08a8eda6ebdebd8189bd6e6c1a1b8be671b030
Jon Ison <jon.c.ison@gmail.com>
2020-06-26 18:18:44 +0100

    Added dummy ViewSet for testing purposes

--- ifbcatsandbox_api/urls.py
+++ ifbcatsandbox_api/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
+# Imports
+# ", include" imports the "include" function
+# "DefaultRouter" is used to generate different routes (endpoints) for any ViewSets
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from ifbcatsandbox_api import views
 
+# A router is used to register required endpoints for API ViewSets
+# e.g. "list" requests use the root of the API, whereas "update", "delete" etc. must access a specific object
+# 'testviewset' is the URL we're regstering - the router will create URLs for us, so don't need to give a trailing '/'
+# "base_name" is used if/wehn URLs are retrieved using URL-retrieving function provided by Django
+router = DefaultRouter()
+router.register('testviewset', views.TestViewSet, base_name='testviewset')
+
+# APIView ("changelog" below) endpoints are registered differently than API ViewSet
+# "include" function is used to include a list of URLS into the URL pattern
+# It figures out the URLs for all the functions ("list" etc.) defined in views.py for the ViewSet - hence the blank string as 1st arg.
 urlpatterns = [
 path('changelog/', views.ChangelogView.as_view()),
+path('', include(router.urls))
 ]
--- ifbcatsandbox_api/views.py
+++ ifbcatsandbox_api/views.py
@@ -4,6 +4,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+
 from ifbcatsandbox_api import serializers
 
 # ChangelogView is just a test API View, done in case APIView(s) are needed in the ifbcatsandbox API
@@ -31,7 +33,7 @@ class ChangelogView(APIView):
         # "data=request.data" assigns the data passed in from the POST request, to the serializer
         serializer = self.serializer_class(data=request.data)
 
-        # is_valid() function valdates the data as per the definition in serializers.py
+        # is_valid() function valdates the data as per the definition in serializers.py (where "testinput" is defined)
         if(serializer.is_valid()):
             testinput = serializer.validated_data.get('testinput')
             message = f'Test input was: {testinput}'
@@ -57,3 +59,61 @@ class ChangelogView(APIView):
     def delete(self, request, pk=None):
         """Dummy "delete" method (to delete an object)."""
         return Response({'method called': 'DELETE'})
+
+
+
+# TestViewSet is just a test API ViewSet
+class TestViewSet(viewsets.ViewSet):
+    """Test API ViewSet.  Currently just returns the ifbcatsandbox API changelog."""
+
+    # Note - reusing the serializer that was used for the "Changelog" APIView - this is OK!
+    serializer_class = serializers.ChangelogSerializer
+
+    # "list" method returns list of objects.  The list request corresponds to the root of the API.
+    def list(self, request):
+        """Dummy "list" method (to list set of objects that ViewSet represents)"""
+        message = [
+        "Test message for list method on APIViewSet.",
+        ]
+
+        return Response({'message': message})
+
+
+    # "create" method is used to create new objects
+    def create(self, request):
+        """Dummy "create" method (to create a new object)"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            testinput = serializer.validated_data.get('testinput')
+            message = f'testinput: {testinput}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    # "retrieve" function is for retrieving a specific object in the ViewSet
+    # A primary key (pk) ID is passed into the URL / request
+    def retrieve(self, request, pk=None):
+        """Dummy "retrieve" function (to get an object by its ID)"""
+        return Response({'http_method:': 'GET'})
+
+    # "update" function is for updating a specific object in the ViewSet
+    # A primary key (pk) ID is  passed into the URL / request
+    def update(self, request, pk=None):
+        """Dummy "update" function (to update an object)"""
+        return Response({'http_method:': 'PUT'})
+
+    # "partial_update" function is for partially updating a specific object in the ViewSet
+    # A primary key (pk) ID is passed into the URL / request
+    def partial_update(self, request, pk=None):
+        """Dummy "partial_update" function (to partially update an object)"""
+        return Response({'http_method:': 'PATCH'})
+
+    # "destroy" function is for deleting a specific object in the ViewSet
+    # A primary key (pk) ID is passed into the URL / request
+    def destroy(self, request, pk=None):
+        """Dummy "destroy" function (to delete an object)"""
+        return Response({'http_method:': 'DELETE'})
