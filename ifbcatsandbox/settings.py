4f8fe09af88c93c985aeb664b208db948765d74c
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-07-17 09:48:10 +0200

    set up config to override drf tempaltes, override input.html template to take into account min and max value

--- ifbcatsandbox/settings.py
+++ ifbcatsandbox/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_templates_patch',
     'rest_framework',
     'rest_framework.authtoken',
     'ifbcatsandbox_api',
