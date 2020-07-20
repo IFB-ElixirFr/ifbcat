0dfe602df427504b54b918d24851fff049076bae
Jon Ison <jon.c.ison@gmail.com>
2020-07-20 14:10:25 +0100

    Adding Community model to models.py including controlled vocabulary of community names.  Note - refers to Organisation model which hasn't been added yet.

--- ifbcatsandbox_api/models.py
+++ ifbcatsandbox_api/models.py
@@ -385,3 +385,47 @@ class ElixirPlatform(models.Model):
     homepage = models.URLField(max_length=255, help_text="Homepage of the ELIXR Platform.")
     coordinator = models.OneToOneField(UserProfile, related_name='elixirPlatformCoordinator', null=True, on_delete=models.SET_NULL, help_text="Coordinator of the ELIXIR Platform activities in France.")
     deputies = models.ManyToManyField(UserProfile, related_name='elixirPlatformDeputies', help_text="Deputy coordinator of the ELIXIR Platform activities in France.")
+
+
+    def __str__(self):
+        """Return the ElixirPlatform model as a string."""
+        return self.name
+
+
+# ELIXIR Platform model
+class Community(models.Model):
+    """Community model: A group of people collaborating on a common scientific or technical topic, including formal ELIXIR  Communities, emerging ELIXIR communities, ELXIR focus groups, IFB communities, and others."""
+
+    # EventType: Controlled vocabulary of types of events.
+    class CommunityName(models.TextChoices):
+        """Controlled vocabulary of names of communities."""
+        # NB: Note "BIOINFO not 3D-BIOINFO" because of Python variable name restrictions.
+        BIOINFO = '3D-BioInfo', _('3D-BioInfo')
+        GALAXY = 'Galaxy', _('Galaxy')
+        INTRINSICALLY_DISORDERED_PROTEINS = 'Intrinsically Disordered Proteins', _('Intrinsically Disordered Proteins')
+        MARINE_METAGENOMICS = 'Marine Metagenomics', _('Marine Metagenomics')
+        METABOLOMICS = 'Metabolomics', _('Metabolomics')
+        MICROBIAL_BIOTECHNOLOGY = 'Microbial Biotechnology', _('Microbial Biotechnology')
+        PLANT_SCIENCES = 'Plant Sciences', _('Plant Sciences')
+        PROTEOMICS = 'Proteomics', _('Proteomics')
+        FEDERATED_HUMAN_DATA = 'Federated Human Data', _('Federated Human Data')
+        HUMAN_COPY_NUMBER_VARIATION = 'Human Copy Number Variation', _('Human Copy Number Variation')
+        RARE_DISEASES = 'Rare Diseases', _('Rare Diseases')
+
+
+    # name, description & homepage are mandatory
+    # null=True is set for coordinator, in case the UserProfile of the coordinator is deleted.
+    name = models.CharField(
+        max_length=255,
+        choices=CommunityName.choices,
+        unique=True,
+        help_text="Name of the community, e.g. 'Galaxy'.")
+
+    description = models.TextField(help_text="Short description of the community.")
+    homepage = models.URLField(max_length=255, help_text="Homepage of the community.")
+    organisations = models.ManyToManyField(Organisation, related_name='communities', help_text="An organisation to which the community is affiliated.")
+
+
+    def __str__(self):
+        """Return the Community model as a string."""
+        return self.name
