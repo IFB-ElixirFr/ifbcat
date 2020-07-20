f21f345a3fbab362b4b6ea933c62ba2b83497560
Jon Ison <jon.c.ison@gmail.com>
2020-07-20 13:46:01 +0100

    Adding ElixirPlatform model to models.py including controlled vocabulary of ELIXIR platform names.  Also added doc strings to other controlled vocabularies.

--- ifbcatsandbox_api/models.py
+++ ifbcatsandbox_api/models.py
@@ -224,6 +224,7 @@ class EventCost(models.Model):
 
     # CostType: Controlled vocabulary of monetary costs to attend an event.
     class CostType(models.TextChoices):
+        """Controlled vocabulary of monetary costs to attend an event."""
         # FREE = 'FR', _('Free')
         # FREE_TO_ACADEMICS = 'FA', _('Free to academics')
         # CONCESSIONS_AVAILABLE = 'CO', _('Concessions available')
@@ -265,9 +266,10 @@ class Event(models.Model):
     # See https://docs.djangoproject.com/en/3.0/topics/i18n/translation/#internationalization-in-python-code
 
     # EventType: Controlled vocabulary of types of events.
-    # Not using both short-form names and human-readable labels, because of issue:
+    # Short-form and human-readable labels are set the same (rather than using an abbreviation for the short-form), because of issue:
     # see https://github.com/joncison/ifbcat-sandbox/pull/9
     class EventType(models.TextChoices):
+        """Controlled vocabulary of types of events."""
         # WORKSHOP = 'WO', _('Workshop')
         # TRAINING_COURSE = 'TR', _('Training course')
         # MEETING = 'ME', _('Meeting')
@@ -280,6 +282,7 @@ class Event(models.Model):
 
     # EventAccessibilityType: Controlled vocabulary for whether an event is public or private.
     class EventAccessibilityType(models.TextChoices):
+        """Controlled vocabulary for whether an event is public or private."""
         # PUBLIC = 'PU', _('Public')
         # PRIVATE = 'PR', _('Private')
         PUBLIC = 'Public', _('Public')
@@ -354,3 +357,31 @@ class EventDate(models.Model):
     def __str__(self):
         """Return the EventDate model as a string."""
         return self.dateStart.__str__()
+
+
+
+# ELIXIR Platform model
+class ElixirPlatform(models.Model):
+    """ELIXIR Platform model: An official ELIXIR Platform, bringing together experts to define the strategy and provide services in a particular area."""
+
+    # EventType: Controlled vocabulary of types of events.
+    class ElixirPlatformName(models.TextChoices):
+        """Controlled vocabulary of names of ELIXIR Platforms."""
+        DATA = 'Data', _('Data')
+        TOOLS = 'Tools', _('Tools')
+        COMPUTE = 'Compute', _('Compute')
+        INTEROPERABILITY = 'Interoperability', _('Interoperability')
+        TRAINING = 'Training', _('Training')
+
+    # name, description, homepage & coordinator are mandatory
+    # null=True is set for coordinator, in case the UserProfile of the coordinator is deleted.
+    name = models.CharField(
+        max_length=255,
+        choices=ElixirPlatformName.choices,
+        unique=True,
+        help_text="Name of the ELIXIR Platform, e.g. 'Tools'.")
+
+    description = models.TextField(help_text="Short description of the ELIXIR Platform.")
+    homepage = models.URLField(max_length=255, help_text="Homepage of the ELIXR Platform.")
+    coordinator = models.OneToOneField(UserProfile, related_name='elixirPlatformCoordinator', null=True, on_delete=models.SET_NULL, help_text="Coordinator of the ELIXIR Platform activities in France.")
+    deputies = models.ManyToManyField(UserProfile, related_name='elixirPlatformDeputies', help_text="Deputy coordinator of the ELIXIR Platform activities in France.")
