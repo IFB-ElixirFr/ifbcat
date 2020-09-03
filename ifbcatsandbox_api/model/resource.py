4b69efd4f86ffd9db116c1971bd788d2dc2ab3c8
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-09-03 21:17:11 +0200

    Make Resources model abstract, allowing child classes to have uniqueness of name independent one to another. Closes #91

--- ifbcatsandbox_api/model/resource.py
+++ ifbcatsandbox_api/model/resource.py
@@ -11,15 +11,18 @@ from ifbcatsandbox_api.validators import validate_can_be_looked_up
 class Resource(models.Model):
     """Resource model: A computing facility, database, tool or training material provided by a bioinformatics team."""
 
+    class Meta:
+        abstract = True
+
     user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
 
-    name = models.CharField(max_length=255, help_text="Name of the resource.", validators=[validate_can_be_looked_up,],)
-    description = models.TextField(help_text="A short description of the resource.")
-    communities = models.ManyToManyField(
-        Community, blank=True, related_name='resources', help_text="Community which uses the resource."
+    name = models.CharField(
+        max_length=255, unique=True, help_text="Name of the resource.", validators=[validate_can_be_looked_up,],
     )
+    description = models.TextField(help_text="A short description of the resource.")
+    communities = models.ManyToManyField(Community, blank=True, help_text="Community which uses the resource.")
     elixirPlatforms = models.ManyToManyField(
-        ElixirPlatform, blank=True, related_name='resources', help_text="ELIXIR Platform which uses the resource."
+        ElixirPlatform, blank=True, help_text="ELIXIR Platform which uses the resource."
     )
 
     def __str__(self):
