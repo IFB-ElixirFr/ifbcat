e84870b71321023b259332360dc856e849479874
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-09-11 12:32:15 +0200

    Using openapi, rendering it with swagger-ui

--- ifbcatsandbox/urls.py
+++ ifbcatsandbox/urls.py
@@ -19,9 +19,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ifbcatsandbox_api.urls')),
     path('', RedirectView.as_view(url='/api/', permanent=False)),
+    path(
+        'swagger-ui/',
+        TemplateView.as_view(template_name='swagger-ui.html', extra_context={'schema_url': 'openapi-schema'}),
+        name='swagger-ui',
+    ),
+    path(
+        'openapi',
+        get_schema_view(title="Your Project", description="API for all things …", version="1.0.0"),
+        name='openapi-schema',
+    ),
 ]
