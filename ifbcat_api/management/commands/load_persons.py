1c15499a397b915959ecea92bd25a96712de2249
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-09-29 12:55:27 +0200

    load persons

--- ifbcat_api/management/commands/load_persons.py
+++ ifbcat_api/management/commands/load_persons.py
@@ -1,51 +1,47 @@
-import os
-import csv
+import logging
+
+import pandas as pd
 from django.core.management import BaseCommand
-from database.models import People
-from catalogue.settings import BASE_DIR
+
+from ifbcat_api import models
+from ifbcat_api.management.commands.load_bioinformatics_teams import to_none_when_appropriate
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    def import_poeple_from_csv_file(self):
-        data_folder = os.path.join(BASE_DIR, 'import_data', 'resources/csv_file')
-        People.objects.all().delete()
-        print(data_folder, 'data_folder')
-        for data_file in os.listdir(data_folder):
-            # name of the correct csv file
-            if data_file == "persons.csv":
-                with open(os.path.join(data_folder, data_file), encoding='utf-8') as data_file:
-                    data = csv.reader(data_file)
-                    for data_object in data:
-                        l = data.line_num
-                        if data_object == []:
-                            data_object = next(data)  # Check for empty lines
-                        if l == 1:
-                            data_object = next(data)
-                        people_name = data_object[0]
-                        people_email = data_object[1]
-                        people_link = str(
-                            "https://www.france-bioinformatique.fr/fr/users/" + people_name.replace(" ", "-")
-                        ).lower()
-                        people_platform = data_object[2]
+    help = "Import Teams"
 
-                        people = ""
-                        try:
-                            people, created = People.objects.get_or_create(
-                                name=people_name,
-                                email=people_email,
-                                link=people_link,
-                                # platform = people_platform,
-                            )
-                            if created:
-                                people.save()
-
-                        except Exception as ex:
-                            print(str(ex))
-                            msg = "\n\nSomething went wrong saving this tool: {}\n{}".format(people, str(ex))
-                            print(msg)
+    def add_arguments(self, parser):
+        parser.add_argument("file", type=str, help="Path to the CSV source file")
 
     def handle(self, *args, **options):
-        """
-        Call the function to import data
-        """
-        self.import_poeple_from_csv_file()
+        df = pd.read_csv(options["file"], sep=",")
+        # models.UserProfile.objects.all().delete()
+        for index, row in df.iterrows():
+            try:
+                try:
+                    bt = models.UserProfile.objects.get(
+                        firstname__unaccent__iexact=row["firstname"].strip(),
+                        lastname__unaccent__iexact=row["lastname"].strip(),
+                    )
+                except models.UserProfile.DoesNotExist:
+                    bt = models.UserProfile()
+                    bt.firstname = row["firstname"].strip()
+                    bt.lastname = row["lastname"].strip()
+                email = row["email"].strip()
+                if email != "todo@todo.com":
+                    bt.email = row["email"].strip()
+                elif bt.email is None or bt.email == "":
+                    bt.email = '%s.%s@todo.com' % (
+                        bt.firstname.strip().replace(" ", "-"),
+                        bt.lastname.strip().replace(" ", "-"),
+                    )
+                    logger.warning("Missing email, building fake one: %s" % bt.email)
+                else:
+                    logger.info("Ignoring fake email %s" % email)
+                bt.homepage = to_none_when_appropriate(row["homepage"])
+                bt.save()
+            except Exception as e:
+                logger.error("Failed with line %i:%s" % (index, str(row)))
+                raise e
