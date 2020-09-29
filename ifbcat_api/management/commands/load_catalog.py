7883fe4b617656b37e2b64f44fcd451c6bb14726
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-09-29 12:31:42 +0200

    load person first

--- ifbcat_api/management/commands/load_catalog.py
+++ ifbcat_api/management/commands/load_catalog.py
@@ -1,21 +1,23 @@
+import os
+
 from django.core.management import BaseCommand
 from django.core.management import call_command
 
-from catalogue.settings import BASE_DIR
-
 
 class Command(BaseCommand):
+    import_data = "./import_data"
+
     def import_catalog(self):
-        call_command('load_platforms')
-        call_command('load_databases')
-        # call_command('load_tools')
-        call_command('load_services')
-        call_command('load_expertises')
-        call_command('load_persons')
-        call_command('load_training')
-        call_command('load_training_material')
-        call_command('load_events')
-        call_command('load_infrastructure')
+        call_command('load_persons', os.path.join(self.import_data, "persons.csv"))
+        call_command('load_bioinformatics_teams', os.path.join(self.import_data, "platforms.csv"))
+        # call_command('load_databases')
+        # # call_command('load_tools')
+        # call_command('load_services')
+        # call_command('load_expertises')
+        # call_command('load_training')
+        # call_command('load_training_material')
+        # call_command('load_events')
+        # call_command('load_infrastructure')
 
     def handle(self, *args, **options):
         """
