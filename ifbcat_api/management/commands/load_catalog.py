eed2c2767f4d41ad7145b7d55daeab4009c4e86d
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2020-10-26 09:10:54 +0100

    adapt load_events to new model

--- ifbcat_api/management/commands/load_catalog.py
+++ ifbcat_api/management/commands/load_catalog.py
@@ -5,18 +5,23 @@ from django.core.management import call_command
 
 
 class Command(BaseCommand):
-    import_data = "./import_data"
+    import_data = "../ifbcat-importdata"
 
     def import_catalog(self):
-        call_command('load_persons', os.path.join(self.import_data, "persons.csv"))
+        call_command(
+            'load_persons',
+            os.path.join(self.import_data, "persons.csv"),
+            os.path.join(self.import_data, "drupal_db_dump/users.txt"),
+        )
         call_command('load_bioinformatics_teams', os.path.join(self.import_data, "platforms.csv"))
-        call_command('load_expertises', os.path.join(self.import_data, "expertises.csv"))
+        call_command('load_biotools')
+        call_command('load_events', os.path.join(self.import_data, "events.csv"))
+        # call_command('load_expertises', os.path.join(self.import_data, "expertises.csv"))
         # call_command('load_databases')
         # # call_command('load_tools')
         # call_command('load_services')
         # call_command('load_training')
         # call_command('load_training_material')
-        # call_command('load_events')
         # call_command('load_infrastructure')
 
     def handle(self, *args, **options):
--- ifbcat_api/management/commands/load_events.py
+++ ifbcat_api/management/commands/load_events.py
@@ -3,82 +3,79 @@ import os
 import csv
 import pytz
 from tqdm import tqdm
-
 from django.db.transaction import atomic
 from django.utils.timezone import make_aware
-
 from django.core.management import BaseCommand
-from database.models import Event
-from catalogue.settings import BASE_DIR
+from ifbcat_api.model.event import *
 
 
 class Command(BaseCommand):
-    def import_events_from_csv_file(self):
-        data_folder = os.path.join(BASE_DIR, 'import_data', 'resources/csv_file')
-        Event.objects.all().delete()
-        # print(data_folder, 'data_folder')
-        for data_file in os.listdir(data_folder):
-            # name of the correct csv file
-            if data_file == "events.csv":
-                with open(os.path.join(data_folder, data_file), encoding='utf-8') as data_file:
-                    print(data_file.name)
-                    data = csv.reader(data_file)
-                    # skip first line as there is always a header
-                    next(data)
-                    # count number of lines
-                    data_len = len(list(data))
-                    data_file.seek(0)
-                    next(data)
-                    # do the work
-                    for data_object in tqdm(data, total=data_len):
-                        if data_object == []:
-                            continue  # Check for empty lines
-                        event_name = data_object[0]
-                        event_type = data_object[1]
-                        event_description = data_object[2]
-                        if data_object[3]:
-                            if "to" in data_object[3]:
-                                event_start_date = datetime.datetime.strptime(
-                                    data_object[3].split(" to ")[0], "%d-%m-%Y"
-                                )  # .strftime("%Y-%m-%d")
-                                event_start_date = make_aware(event_start_date, timezone=pytz.timezone('Europe/Paris'))
-                                event_end_date = datetime.datetime.strptime(data_object[3].split(" to ")[1], "%d-%m-%Y")
-                                event_end_date = make_aware(event_end_date, timezone=pytz.timezone('Europe/Paris'))
-                            else:
-                                event_start_date = datetime.datetime.strptime(
-                                    data_object[3], "%d-%m-%Y"
-                                )  # .strftime("%Y-%m-%d")
-                                event_start_date = make_aware(event_start_date, timezone=pytz.timezone('Europe/Paris'))
-                                event_end_date = None
-                        else:
-                            event_start_date = None
-                            event_end_date = None
-
-                        event_location = data_object[4]
-                        event_link = data_object[5]
-                        event_organizer = data_object[6]
-                        event_sponsors = data_object[7]
-                        event_logo = data_object[8]
-
-                        try:
-                            event = Event.objects.create(
-                                name=event_name,
-                                logo=event_logo,
-                                event_type=event_type,
-                                description=event_description,
-                                start_date=event_start_date,
-                                end_date=event_end_date,
-                                location=event_location,
-                                link=event_link,
-                                organizer=event_organizer,
-                                sponsors=event_sponsors,
-                            )
-                        except Exception as e:
-                            print(data_object)
-                            raise e
+    def add_arguments(self, parser):
+        parser.add_argument("file", type=str, help="Path to the CSV source file")
 
     def handle(self, *args, **options):
-        """
-        Call the function to import data
-        """
-        self.import_events_from_csv_file()
+        Event.objects.all().delete()
+        # print(data_folder, 'data_folder')
+        with open(options["file"], encoding='utf-8') as data_file:
+            print(data_file.name)
+            data = csv.reader(data_file)
+            # skip first line as there is always a header
+            next(data)
+            # count number of lines
+            data_len = len(list(data))
+            data_file.seek(0)
+            next(data)
+            # do the work
+            for data_object in tqdm(data, total=data_len):
+                if data_object == []:
+                    continue  # Check for empty lines
+                event_name = data_object[0]
+                event_type = data_object[1]
+                event_description = data_object[2]
+                if data_object[3]:
+                    if "to" in data_object[3]:
+                        event_start_date = datetime.datetime.strptime(
+                            data_object[3].split(" to ")[0], "%d-%m-%Y"
+                        )  # .strftime("%Y-%m-%d")
+                        event_start_date = make_aware(event_start_date, timezone=pytz.timezone('Europe/Paris'))
+                        event_end_date = datetime.datetime.strptime(data_object[3].split(" to ")[1], "%d-%m-%Y")
+                        event_end_date = make_aware(event_end_date, timezone=pytz.timezone('Europe/Paris'))
+                    else:
+                        event_start_date = datetime.datetime.strptime(
+                            data_object[3], "%d-%m-%Y"
+                        )  # .strftime("%Y-%m-%d")
+                        event_start_date = make_aware(event_start_date, timezone=pytz.timezone('Europe/Paris'))
+                        event_end_date = None
+                else:
+                    event_start_date = None
+                    event_end_date = None
+
+                event_location = data_object[4]
+                event_link = data_object[5]
+                event_organizer = data_object[6]
+                event_sponsors = data_object[7]
+                event_logo = data_object[8]
+
+                try:
+                    event = Event.objects.create(
+                        name=event_name,
+                        logo_url=event_logo,
+                        type=event_type,
+                        description=event_description,
+                        city=event_location,
+                        homepage=event_link,
+                        # organizer=event_organizer,
+                        # sponsoredBy=event_sponsors,
+                    )
+                    event.save()
+
+                    dates = EventDate.objects.create(dateStart=event_start_date, dateEnd=event_end_date)
+                    event.dates.add(dates)
+
+                    for organizer in event_organizer.split(','):
+                        organisation = Organisation.objects.get_or_create(name=organizer)
+                        event.organisedByOrganisations.add(organisation)
+
+                except Exception as e:
+                    print(data_object)
+                    raise e
