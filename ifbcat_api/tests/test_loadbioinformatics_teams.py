674d7c1163db4853a05fc6336d2ab4295f7fbd67
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-11-09 16:10:50 +0100

    adding a basic test that check all resources are loaded

--- ifbcat_api/tests/test_loadbioinformatics_teams.py
+++ ifbcat_api/tests/test_loadbioinformatics_teams.py
@@ -3,7 +3,6 @@ import os
 from unittest.case import skipIf
 
 from django.core import management
-from django.test import TestCase
 
 from ifbcat_api.model.organisation import Organisation
 from ifbcat_api.model.team import Team
@@ -12,6 +11,7 @@ from ifbcat_api.tests.test_with_importdata import EnsureImportDataAreHere
 logger = logging.getLogger(__name__)
 
 
+@skipIf(True, "use test_loadcatalog instead, kept as an example")
 class LoadBioinfoTeamsTestCase(EnsureImportDataAreHere):
     def test_import_in_db(self):
         management.call_command('load_bioinformatics_teams', os.path.join(self.import_data, "platforms.csv"))
