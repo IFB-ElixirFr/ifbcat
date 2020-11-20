caa8104c9e66f5ccf67b924a8dc4cfddc8e6bcf7
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-11-20 10:53:36 +0100

    add a group that contains all permissions for ifb_cat, except the permission on UserProfil

--- ifbcat_api/business_logic.py
+++ ifbcat_api/business_logic.py
@@ -1,6 +1,7 @@
 import logging
 
-from django.contrib.auth import get_permission_codename
+from django.apps import apps
+from django.contrib.auth import get_permission_codename, get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
@@ -10,6 +11,8 @@ from ifbcat_api.model.userProfile import UserProfile
 logger = logging.getLogger(__name__)
 
 __USER_MANAGER_GRP_NAME = "User manager"
+
+__NO_RESTRICTION = "Grant access to all models of the catalog"
 
 
 def __get_user_manager_group():
@@ -25,8 +28,22 @@ def __get_user_manager_group():
     return g
 
 
+def __get_no_restriction_on_catalog_models_group():
+    g, created = Group.objects.get_or_create(name=__NO_RESTRICTION)
+    return g
+
+
 def init_business_logic():
     __get_user_manager_group()
+    no_restriction = __get_no_restriction_on_catalog_models_group()
+    ifbcat_api_models = apps.get_app_config('ifbcat_api').get_models()
+    for model in ifbcat_api_models:
+        if model == get_user_model():
+            continue
+        for p in Permission.objects.filter(
+            content_type=ContentType.objects.get_for_model(model),
+        ):
+            no_restriction.permissions.add(p)
 
 
 ###############################################################################
@@ -49,6 +66,24 @@ def set_user_manager(user, status: bool):
 
 def can_edit_user(acting_user, edited_user):
     return acting_user.groups.filter(name=__USER_MANAGER_GRP_NAME).exists()
+
+
+###############################################################################
+# No restriction on catalog's models
+###############################################################################
+def has_no_restriction_on_catalog_models(user, request=None):
+    return user.groups.filter(name=__NO_RESTRICTION).exists()
+
+
+def get_no_restriction_on_catalog_models_name():
+    return __NO_RESTRICTION
+
+
+def set_no_restriction_on_catalog_models(user, status: bool):
+    if status:
+        user.groups.add(__get_user_manager_group())
+    else:
+        user.groups.remove(__get_user_manager_group())
 
 
 ###############################################################################
