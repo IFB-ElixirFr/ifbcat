29a933954339b51d969bf3e2d34264989ca99330
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2020-12-09 10:40:08 +0100

    fix permission so user manager can access

--- ifbcat_api/permissions.py
+++ ifbcat_api/permissions.py
@@ -161,7 +161,9 @@ class PubliclyReadableByUsersEditableBySuperuser(permissions.BasePermission):
 
 class PubliclyReadableEditableByUserManager(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_superuser
+        from ifbcat_api import business_logic
+
+        return business_logic.is_user_manager(request.user)
 
     def has_object_permission(self, request, view, obj):
         from ifbcat_api import business_logic
