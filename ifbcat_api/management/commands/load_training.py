432fc5283610c60fb04d7a311412854bb1c24cbb
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2020-12-14 15:53:55 +0100

    add load_training

--- ifbcat_api/management/commands/load_training.py
+++ ifbcat_api/management/commands/load_training.py
@@ -1,6 +1,8 @@
 import os
 import csv
 import pytz
+import logging
+import pandas as pd
 
 from django.db.transaction import atomic
 from django.utils.timezone import make_aware
@@ -8,14 +10,38 @@ import datetime
 from django.core.management import BaseCommand
 from ifbcat_api.models import TrainingEvent
 from ifbcat_api.models import Keyword
+from ifbcat_api.model.organisation import Organisation
+from ifbcat_api.model.team import Team
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("file", type=str, help="Path to the CSV source file")
+        parser.add_argument(
+            "--trainings",
+            default="import_data/training.csv",
+            type=str,
+            help="Path to the CSV source file",
+        )
+        parser.add_argument(
+            "--mapping-organisations",
+            default="import_data/manual_curation/mapping_organisations.csv",
+            type=str,
+            help="Path to the CSV file containing mapping for organisations between Drupal names and Ifbcat ones.",
+        )
+        parser.add_argument(
+            "--mapping-teams",
+            default="import_data/manual_curation/mapping_teams.csv",
+            type=str,
+            help="Path to the CSV file containing mapping for teams between Drupal names and Ifbcat ones.",
+        )
 
     def handle(self, *args, **options):
-        with open(os.path.join(options["file"]), encoding='utf-8') as data_file:
+        mapping_organisations = pd.read_csv(options["mapping_organisations"], sep=",")
+        mapping_teams = pd.read_csv(options["mapping_teams"], sep=",")
+
+        with open(os.path.join(options["trainings"]), encoding='utf-8') as data_file:
             data = csv.reader(data_file)
             # skip first line as there is always a header
             next(data)
@@ -23,13 +49,13 @@ class Command(BaseCommand):
             for data_object in data:
                 if data_object == []:
                     continue  # Check for empty lines
-                formation_name = data_object[0]
-                formation_type = data_object[2]
-                formation_description = data_object[4]
-                formation_keywords = data_object[5].split(",")
-                formation_keywords_list = []
-                formation_keyword = ""
-                for keyword in formation_keywords:
+                training_name = data_object[0]
+                training_type = data_object[2]
+                training_description = data_object[4]
+                training_keywords = data_object[5].split("\n")
+                training_keywords_list = []
+                training_keyword = ""
+                for keyword in training_keywords:
                     keyword = keyword.strip()
                     if keyword == 'Aucun des termes ci-dessus ne convient':
                         continue
@@ -37,115 +63,146 @@ class Command(BaseCommand):
 
                         try:
 
-                            formation_keyword, created = Keyword.objects.get_or_create(
+                            training_keyword, created = Keyword.objects.get_or_create(
                                 keyword=keyword,
                             )
-                            formation_keyword.save()
-                            formation_keywords_list.append(formation_keyword)
+                            training_keyword.save()
+                            training_keywords_list.append(training_keyword)
                             display_format = '\nKeyword "{}" has been saved.'
-                            print(display_format.format(formation_keyword))
+                            print(display_format.format(training_keyword))
                         except Exception as ex:
                             print(str(ex))
                             msg = "\n\nSomething went wrong saving this keyword: {}\n{}".format(
-                                formation_keyword, str(ex)
+                                training_keyword, str(ex)
                             )
                             print(msg)
 
                 if data_object[7]:
                     if "to" in data_object[7]:
-                        formation_start_date = datetime.datetime.strptime(
+                        training_start_date = datetime.datetime.strptime(
                             data_object[7].split(" to ")[0], "%d-%m-%Y"
                         )  # .strftime("%Y-%m-%d")
-                        formation_start_date = make_aware(formation_start_date, timezone=pytz.timezone('Europe/Paris'))
-                        formation_end_date = datetime.datetime.strptime(data_object[7].split(" to ")[1], "%d-%m-%Y")
-                        formation_end_date = make_aware(formation_end_date, timezone=pytz.timezone('Europe/Paris'))
+                        training_start_date = make_aware(training_start_date, timezone=pytz.timezone('Europe/Paris'))
+                        training_end_date = datetime.datetime.strptime(data_object[7].split(" to ")[1], "%d-%m-%Y")
+                        training_end_date = make_aware(training_end_date, timezone=pytz.timezone('Europe/Paris'))
                     else:
-                        formation_start_date = datetime.datetime.strptime(
+                        training_start_date = datetime.datetime.strptime(
                             data_object[7], "%d-%m-%Y"
                         )  # .strftime("%Y-%m-%d")
-                        formation_start_date = make_aware(formation_start_date, timezone=pytz.timezone('Europe/Paris'))
-                        formation_end_date = None
+                        training_start_date = make_aware(training_start_date, timezone=pytz.timezone('Europe/Paris'))
+                        training_end_date = None
                 else:
-                    formation_start_date = None
-                    formation_end_date = None
-                formation_acces_condition = data_object[8]
-                formation_link = data_object[9]
-                formation_location = data_object[10]
-                formation_organizer = data_object[11]
-                formation_sponsors = data_object[12]
+                    training_start_date = None
+                    training_end_date = None
+                training_acces_condition = data_object[8]
+                training_link = data_object[9]
+                training_location = data_object[10]
+                training_organizer = data_object[11]
+                training_sponsors = data_object[12]
                 if data_object[13]:
-                    formation_number_people_trained = data_object[13].split("/ ")[0]
+                    training_number_people_trained = data_object[13].split("/ ")[0]
                 else:
-                    formation_number_people_trained: None
+                    training_number_people_trained: None
                 if data_object[14]:
-                    formation_number_of_academic_participants = data_object[14]
+                    training_number_of_academic_participants = data_object[14]
                 else:
-                    formation_number_of_academic_participants = None
+                    training_number_of_academic_participants = None
                 if data_object[15]:
-                    formation_number_of_non_academic_participants = data_object[15]
+                    training_number_of_non_academic_participants = data_object[15]
                 else:
-                    formation_number_of_non_academic_participants = None
+                    training_number_of_non_academic_participants = None
 
                 if data_object[16]:
-                    formation_training_time = data_object[16]
+                    training_training_time = data_object[16]
                 else:
-                    formation_training_time = None
-                formation_participation = data_object[17]
-                formation_training_level = data_object[18]
-                formation_training_operator = data_object[19]
+                    training_training_time = None
+                training_participation = data_object[17]
+                training_training_level = data_object[18]
+                training_training_operator = data_object[19]
                 if data_object[20]:
-                    formation_number_of_sessions = data_object[20]
+                    training_number_of_sessions = data_object[20]
                 else:
-                    formation_number_of_sessions = None
-                formation_recurrence = data_object[21]
+                    training_number_of_sessions = None
+                training_recurrence = data_object[21]
                 if data_object[22]:
-                    formation_satisfaction_rate = data_object[22].split("%")[0]
+                    training_satisfaction_rate = data_object[22].split("%")[0]
                 else:
-                    formation_satisfaction_rate = None
-                # formation_platform = data_object[23]
+                    training_satisfaction_rate = None
+                # training_platform = data_object[23]
 
-                formation = ""
+                training = ""
 
                 try:
-                    formation, created = TrainingEvent.objects.get_or_create(
-                        name=formation_name,
+                    training, created = TrainingEvent.objects.get_or_create(
+                        name=training_name,
                         type="Training course",
-                        # formation_type=formation_type,
-                        description=formation_description,
-                        # start_date=formation_start_date,
-                        # end_date=formation_end_date,
-                        city=formation_location,
-                        # access_conditions=formation_acces_condition,
-                        homepage=formation_link,
-                        # organizer=formation_organizer,
-                        # sponsors=formation_sponsors,
-                        # number_people_trained=formation_number_people_trained,
-                        # number_of_academic_participants=formation_number_of_academic_participants,
-                        # number_of_non_academic_participants=formation_number_of_non_academic_participants,
-                        # training_time=formation_training_time,
-                        # participation=formation_participation,
-                        # training_level=formation_training_level,
-                        # training_operator=formation_training_operator,
-                        # number_of_sessions=formation_number_of_sessions,
-                        # recurrence=formation_recurrence,
-                        # satisfaction_rate=formation_satisfaction_rate,
-                        # platform = formation_platform,
+                        # training_type=training_type,
+                        description=training_description,
+                        # start_date=training_start_date,
+                        # end_date=training_end_date,
+                        city=training_location,
+                        # access_conditions=training_acces_condition,
+                        homepage=training_link,
+                        # organizer=training_organizer,
+                        # sponsors=training_sponsors,
+                        # number_people_trained=training_number_people_trained,
+                        # number_of_academic_participants=training_number_of_academic_participants,
+                        # number_of_non_academic_participants=training_number_of_non_academic_participants,
+                        # training_time=training_training_time,
+                        # participation=training_participation,
+                        # training_level=training_training_level,
+                        # training_operator=training_training_operator,
+                        # number_of_sessions=training_number_of_sessions,
+                        # recurrence=training_recurrence,
+                        # satisfaction_rate=training_satisfaction_rate,
+                        # platform = training_platform,
                     )
 
-                    print(formation_organizer)
+                    print(training_organizer)
+                    for organizer in training_organizer.split(','):
+                        organizer = organizer.strip()
+                        if organizer == '':
+                            logger.debug(f'No organizer for {training_name}')
+                        elif Organisation.objects.filter(name=organizer).exists():
+                            organisation = Organisation.objects.get(name=organizer)
+                            training.organisedByOrganisations.add(organisation)
+                        elif organizer in mapping_organisations['drupal_name'].tolist():
+                            organizer_row = mapping_organisations[mapping_organisations['drupal_name'] == organizer]
+                            if not organizer_row['orgid'].isna().iloc[0]:
+                                print(organizer_row['orgid'])
+                                organisation = Organisation.objects.get(orgid=organizer_row['orgid'].iloc[0])
+                            elif not organizer_row['ifbcat_name'].isna().iloc[0]:
+                                print(organizer_row['orgid'])
+                                organisation = Organisation.objects.get(name=organizer_row['ifbcat_name'].iloc[0])
+                            training.organisedByOrganisations.add(organisation)
+
+                        # elif BioinformaticsTeam.objects.filter(name=organizer).exists():
+                        #    team = BioinformaticsTeam.objects.get(name=organizer)
+                        #    event.organisedByBioinformaticsTeams.add(team)
+
+                        elif Team.objects.filter(name=organizer).exists():
+                            team = Team.objects.get(name=organizer)
+                            training.organisedByTeams.add(team)
+                        elif organizer in mapping_teams['drupal_name'].tolist():
+                            organizer_row = mapping_teams[mapping_teams['drupal_name'] == organizer]
+                            team = Team.objects.get(name=organizer_row['ifbcat_name'].iloc[0])
+                            training.organisedByTeams.add(team)
+
+                        else:
+                            logger.error(f'{organizer} is not an organisation in the DB.')
 
                     if created:
-                        # formation.full_clean()
-                        formation.save()
+                        # training.full_clean()
+                        training.save()
 
-                        display_format = "\nFormation, {}, has been saved."
-                        print(display_format.format(formation))
-                        for keyword in formation_keywords_list:
-                            formation.keywords.add(keyword)
+                        display_format = "\ntraining, {}, has been saved."
+                        print(display_format.format(training))
+                        for keyword in training_keywords_list:
+                            training.keywords.add(keyword)
 
-                        formation.save()
+                        training.save()
 
                 except Exception as ex:
                     print(str(ex))
-                    msg = "\n\nSomething went wrong saving this training: {}\n{}".format(formation, str(ex))
+                    msg = "\n\nSomething went wrong saving this training: {}\n{}".format(training, str(ex))
                     print(msg)
