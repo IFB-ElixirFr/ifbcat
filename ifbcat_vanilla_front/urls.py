eed0d203dc5cd99940c7696171c9984e771a7f0d
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2021-03-04 12:26:37 +0100

    Team is platform

--- ifbcat_vanilla_front/urls.py
+++ ifbcat_vanilla_front/urls.py
@@ -5,6 +5,6 @@ from ifbcat_vanilla_front import views
 app_name = 'vfront'
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^team/$', views.TeamListView.as_view(), name='team-list'),
-    url(r'^team/(?P<slug>[-\w ]+)/$', views.TeamDetailView.as_view(), name='team-detail'),
+    url(r'^platform/$', views.TeamListView.as_view(), name='team-list'),
+    url(r'^platform/(?P<slug>[-\w ]+)/$', views.TeamDetailView.as_view(), name='team-detail'),
 ]
