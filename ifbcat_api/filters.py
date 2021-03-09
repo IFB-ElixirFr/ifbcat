a3d3222cae7b38e5a31ad415800a29fb10444478
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2021-03-09 14:23:47 +0100

    overriding get_schema_operation_parameters to retrieve endpoints and/or choices when appropriate (controled by settings.MAX_CHOICES_COUNT_IN_SCHEMA, and presence of an endpoint)

--- ifbcat_api/filters.py
+++ ifbcat_api/filters.py
@@ -9,9 +9,14 @@
 # "IsAuthenticatedOrReadOnly" is used to ensure that a ViewSet is read-only if the user is not autheticated.
 # "IsAuthenticated" is used to block access to an entire ViewSet endpoint unless a user is autheticated
 
+import warnings
+
+from django.conf import settings
 from django.core.exceptions import FieldDoesNotExist
 from django.db.models import Q, ManyToManyField, ManyToOneRel
+from django.urls import reverse, NoReverseMatch
 from django_filters import rest_framework as django_filters
+from django_filters.fields import ModelMultipleChoiceField
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -45,3 +50,51 @@ class AutoSubsetFilterSet(django_filters.FilterSet):
 class DjangoFilterAutoSubsetBackend(DjangoFilterBackend):
     filterset_base = AutoSubsetFilterSet
     raise_exception = True
+
+    def get_schema_operation_parameters(self, view):
+        try:
+            queryset = view.get_queryset()
+        except Exception:
+            queryset = None
+            warnings.warn("{} is not compatible with schema generation".format(view.__class__))
+
+        filterset_class = self.get_filterset_class(view, queryset)
+
+        if not filterset_class:
+            return []
+
+        parameters = []
+        for field_name, field in filterset_class.base_filters.items():
+            parameter = {
+                'name': field_name,
+                'required': field.extra['required'],
+                'in': 'query',
+                'description': field.label if field.label is not None else field_name,
+                'schema': {
+                    'type': 'string',
+                },
+            }
+            if field.extra and 'choices' in field.extra:
+                parameter['schema']['enum'] = [c[0] for c in field.extra['choices']]
+            if field.field_class == ModelMultipleChoiceField:
+                try:
+                    parameter['schema']['endpoint'] = reverse(get_list_view_name(field.queryset.model))
+                    parameter['schema']['type'] = 'id'
+                    need_choices = False
+                except NoReverseMatch:
+                    need_choices = True
+                choices_count_in_schema = settings.MAX_CHOICES_COUNT_IN_SCHEMA
+                if need_choices or choices_count_in_schema == -1 or field.queryset.count() < choices_count_in_schema:
+                    parameter['schema']['type'] = 'id'
+                    parameter['schema']['choices'] = [(o.id, str(o)) for o in field.queryset.all()]
+            parameters.append(parameter)
+        return parameters
+
+
+def get_list_view_name(model):
+    """
+    Given a model class, return the view name to use for URL relationships
+    that refer to instances of the model.
+    inspiration : from rest_framework.utils.field_mapping.get_detail_view_name
+    """
+    return '%(model_name)s-list' % {'app_label': model._meta.app_label, 'model_name': model._meta.object_name.lower()}
