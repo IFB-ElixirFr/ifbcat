a7a8d0df71d58d0a32bcafe964d0f27afd2e3856
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2021-03-15 10:38:43 +0100

    caching grid api calls

--- ifbcat_api/management/commands/load_organisations_from_gridid.py
+++ ifbcat_api/management/commands/load_organisations_from_gridid.py
@@ -1,24 +1,21 @@
 import csv
-import datetime
+import json
 import logging
 import os
-import json
-import urllib3
 import re
+
+import urllib3
+from django.core.management import BaseCommand
 from tqdm import tqdm
 
-import pytz
-from django.core.management import BaseCommand
-from django.utils.timezone import make_aware
-from django.contrib.auth import get_user_model
-
-from ifbcat_api.model.event import *
 from ifbcat_api.model.organisation import Organisation
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    http = None
+
     def add_arguments(self, parser):
         parser.add_argument(
             "--file",
@@ -42,9 +39,7 @@ class Command(BaseCommand):
                 ifbcat_name = data_object[1]
                 orgid = data_object[2]
                 if grid.match(orgid):
-                    http = urllib3.PoolManager()
-                    req = http.request('GET', f'https://www.grid.ac/institutes/{orgid}?format=json')
-                    response = json.loads(req.data.decode('utf-8'))
+                    response = self.get_from_grid_ac(orgid)
 
                     if response['institute']['acronyms']:
                         name = response['institute']['acronyms'][0]
@@ -74,3 +69,27 @@ class Command(BaseCommand):
                         logger.error(o)
                         print('error' + name)
                         raise e
+
+    def get_from_grid_ac(self, orgid):
+        key = None
+        cache_dir = os.environ.get('CACHE_DIR', None)
+        if cache_dir is not None:
+            cache_dir = os.path.join(cache_dir, 'grid.ac')
+            os.makedirs(cache_dir, exist_ok=True)
+            key = f'{orgid}.json'
+            try:
+                with open(os.path.join(cache_dir, key)) as f:
+                    response = json.load(f)
+                return response
+            except FileNotFoundError:
+                pass
+
+        if self.http is None:
+            self.http = urllib3.PoolManager()
+        req = self.http.request('GET', f'https://www.grid.ac/institutes/{orgid}?format=json')
+        response = json.loads(req.data.decode('utf-8'))
+
+        if key is not None:
+            with open(os.path.join(cache_dir, key), 'w') as f:
+                json.dump(response, f)
+        return response
