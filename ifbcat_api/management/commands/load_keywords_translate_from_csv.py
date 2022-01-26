c09741f9f2a0bc3483a8d5b2bcf8b17442b89311
Aziguy <monsieur_hernandez@hotmail.com>
2022-01-26 13:28:07 +0100

    Improve load keywords django command

--- ifbcat_api/management/commands/load_keywords_translate_from_csv.py
+++ ifbcat_api/management/commands/load_keywords_translate_from_csv.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
-            "file", type=str, default="import_data/keywords_english_translate.csv", help="Path to the CSV source file"
+            "--file", type=str, default="import_data/keywords_english_translate.csv", help="Path to the CSV source file"
         )
 
     def handle(self, *args, **options):
@@ -20,13 +20,25 @@ class Command(BaseCommand):
                 raise "The file is not conform!"
             else:
                 count = 0
-                uncount = 0
+                uncounted = 0
+                untranslated = []
                 for line in data:
-                    for cle in Keyword.objects.all().order_by('keyword').values():
-                        if line['English_keywords'] == cle['keyword']:
-                            cle.keyword = str(line['English_keywords'])
+                    for cle in Keyword.objects.filter(keyword=line['French_keywords']):
+                        if cle.keyword != line['French_keywords']:
+                            print(uncounted, cle.keyword)
+                            uncounted += 1
+                            untranslated.append(cle.keyword)
+                            print("non")
+
+                        else:
+                            cle.keyword = line['English_keywords']
                             cle.save()
                             count += 1
-
+                with open("import_data/keywords_english_translate.csv", 'a') as f_object:
+                    writer_object = csv.writer(f_object)
+                    print(untranslated)
+                    for keyword in untranslated:
+                        writer_object.writerow(keyword)
+                    f_object.close()
                 print(str(count) + " Items have been updated")
-                print(str(uncount) + " Items have not been updated")
+                print(str(uncounted) + " Items have been added to the csvfile")
