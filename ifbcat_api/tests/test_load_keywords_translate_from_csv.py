e51c5f0e3c678965a321c58721c9af01e096b288
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2022-02-14 10:30:57 +0100

    more test

--- ifbcat_api/tests/test_load_keywords_translate_from_csv.py
+++ ifbcat_api/tests/test_load_keywords_translate_from_csv.py
@@ -13,12 +13,30 @@ logger = logging.getLogger(__name__)
 
 class TestLoadKeywords(TestCase):
     def test_create_new_file(self):
+        models.Keyword.objects.create(keyword="KW1-fr")
         with NamedTemporaryFile(delete=True, suffix=".csv") as f:
             pass
 
         self.assertFalse(os.path.exists(f.name))
         management.call_command('load_keywords_translate_from_csv', file=f.name)
         self.assertTrue(os.path.exists(f.name))
+
+        with open(f.name, 'r') as data_file:
+            self.assertEqual(
+                len(data_file.readlines()),
+                models.Keyword.objects.count() + 1,
+                "1 plus the number of keyword to translate",
+            )
+
+        models.Keyword.objects.create(keyword="KW2-fr")
+        management.call_command('load_keywords_translate_from_csv', file=f.name)
+
+        with open(f.name, 'r') as data_file:
+            self.assertEqual(
+                len(data_file.readlines()),
+                models.Keyword.objects.count() + 1,
+                "1 plus the number of keyword to translate",
+            )
 
     def test_append_to_empty_file(self):
         models.Keyword.objects.create(keyword="KW1")
