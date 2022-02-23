ac21b9cb78439df0302b7505fc3a8a764d3f4e58
Aziguy <monsieur_hernandez@hotmail.com>
2022-02-23 10:47:35 +0100

    Improve load keywords custom command.

--- ifbcat_api/management/commands/load_keywords_translate_from_csv.py
+++ ifbcat_api/management/commands/load_keywords_translate_from_csv.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import logging
+from tqdm import tqdm
 import os
 from django.core.management import BaseCommand
 from ifbcat_api.models import Keyword
@@ -13,63 +14,22 @@ class Command(BaseCommand):
         parser.add_argument(
             "--file",
             type=str,
-            default="import_data/keywords_english_translate23.csv",
+            default="import_data/keywords_english_translate.csv",
             help="Path to the CSV source file",
         )
 
     def handle(self, *args, **options):
-        if not os.path.exists(options["file"]):
-            logger.warning(f"Cannot find the file named {options['file']} let's create it")
-            with open(os.path.join(options["file"]), 'w+', encoding='utf-8', newline='') as data_file:
-                data = csv.DictWriter(data_file, fieldnames=['French_keywords', 'English_keywords'], delimiter='\t')
-                data.writeheader()
+        if os.path.exists(options["file"]):
+            my_dict = {}
+            with open(options["file"], encoding='utf-8') as data_file:
+                data = csv.reader(data_file, delimiter='\t')
+                data_file.seek(0)
+                next(data)  # skip first line as there is always a header we guests
 
-                qs_dict = Keyword.objects.all().values().order_by('keyword')
-                i = 0
-                for row in qs_dict:
-                    data.writerow({'French_keywords': row['keyword'], 'English_keywords': 'To translate'})
-                    i += 1
-                data_file.close()
-            logger.info(f"{i} keywords have been added to the new file named {options['file']}")
-        else:
-            with open(os.path.join(options["file"]), encoding='utf-8', newline='', mode='r+') as data_file:
-                data = csv.DictReader(data_file, delimiter='\t')
+                for data_object in data:
+                    my_dict[data_object[0]] = data_object[1]
 
-                count = 0
-                untranslated_nb = 0
-                # Queryset that content the keywords of DB
-                qs_dict = Keyword.objects.all().values().order_by('keyword')
-                file_dict = list(data)  # Convert data in csv file
-                # Convert dict file to list and assign to `dict_to_list_file_fr` and `dict_to_list_file_en`
-                dict_to_list_file_fr = [line_file['French_keywords'] for line_file in file_dict]
-                dict_to_list_file_en = [line_file['English_keywords'] for line_file in file_dict]
-                # Convert queryset to list and assign to `dict_to_list_qs`
-                dict_to_list_qs = [item_qs['keyword'] for item_qs in qs_dict]
-                # make the diff and assign to `diff_list` (Content the keywords that we need to translate).
-                diff_list = list((set(dict_to_list_qs) - set(dict_to_list_file_fr)) - set(dict_to_list_file_en))
-
-                untranslated = [[line, 'To translate'] for line in diff_list]  # Contents untranslated keywords
-                writer_object = csv.writer(data_file, delimiter='\t')
-                for elt in untranslated:
-                    writer_object.writerow(elt)
-                    untranslated_nb += 1
-                data_file.close()
-                logger.info(f"{untranslated_nb} Items have been added to the csv file {options['file']}")
-
-                en_word_list = [
-                    row_file['English_keywords'] for row_file in file_dict
-                ]  # Assign english keywords in `en_word_list`
-
-                # if not any(word == 'To translate' for word in en_word_list):
-                for line in file_dict:
-                    key_db = Keyword.objects.filter(keyword__exact=line['French_keywords']).first()
-                    if key_db != line['English_keywords'] and key_db is not None:
-                        if line['English_keywords'] != 'To translate':
-                            key_db.keyword = line['English_keywords']
-                            key_db.save()
-                            count += 1
-                        else:
-                            logger.warning(f"{line['French_keywords']} has to be translated!")
-                    else:
-                        logger.warning(f"{line['French_keywords']} has already been translated!")
-                logger.info(f"{count} Items have been updated")
+            qs_dict = Keyword.objects.all().order_by('keyword')
+            # print(my_dict['Accessibilité de la chromatine'])
+            for key in qs_dict:
+                print(key.keyword, "<->", my_dict.get(key))
