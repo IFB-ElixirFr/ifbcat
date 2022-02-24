d89f78dfe1787d9becfeb3ade801075f56e698f3
Aziguy <monsieur_hernandez@hotmail.com>
2022-02-24 12:50:33 +0100

    Some changes in algorithm for loading keywords.

--- ifbcat_api/management/commands/load_keywords_translate_from_csv.py
+++ ifbcat_api/management/commands/load_keywords_translate_from_csv.py
@@ -14,62 +14,50 @@ class Command(BaseCommand):
         parser.add_argument(
             "--file",
             type=str,
-            default="import_data/keywords_english_translate_test.csv",
+            default="import_data/keywords_english_translate_testok_.csv",
             help="Path to the CSV source file",
         )
 
     def handle(self, *args, **options):
-        if os.path.exists(options["file"]):
-            file_dict = {}
-            count = 0
-            untranslated_nb = 0
-            with open(options["file"], encoding='utf-8') as data_file:
-                data = csv.reader(data_file, delimiter='\t')
-                data_file.seek(0)
-                next(data)  # skip first line as there is always a header we guests
+        if not os.path.exists(options["file"]):
+            logger.error(f"Cannot find the file named {options['file'][12:]}")
+            return
 
-                for data_object in data:
-                    file_dict[data_object[0]] = data_object[1]
+        file_dict = {}
+        count = 0
+        untranslated_nb = 0
+        with open(options["file"], encoding='utf-8') as data_file:
+            data = csv.reader(data_file, delimiter='\t')
+            data_file.seek(0)
+            next(data)  # skip first line as there is always a header we guests
 
-                qs_keyword = Keyword.objects.all().order_by('keyword')
-                for key in qs_keyword:
-                    str_key = key.keyword.strip()
-                    if file_dict.get(str_key) == "To_translate":
-                        pass
-                    elif str_key in file_dict.keys() and str_key not in file_dict.values():
-                        key.keyword = file_dict.get(str_key)
-                        key.save()
-                        count += 1
-                    elif str_key in file_dict.values():
-                        logger.info(f"{str_key} has already been translated!")
-                    else:
-                        print(untranslated_nb, str_key)
-                        print("___________")
-                        file_dict[str_key] = "To_translate"
-                        untranslated_nb += 1
+            for data_object in data:
+                file_dict[data_object[0]] = data_object[1]
 
-                # f = open(options["file"], 'r+')
-                # f.truncate(0)
-                print(file_dict)
-                with open(os.path.join(options["file"]), 'w', newline='') as csv_file:
-                    writer = csv.DictWriter(
-                        csv_file, fieldnames=['French_keywords', 'English_keywords'], delimiter='\t'
-                    )
-                    writer.writeheader()
-                    for key in file_dict:
-                        writer.writerow({'French_keywords': key, 'English_keywords': file_dict[key]})
+            qs_keyword = Keyword.objects.all().order_by('keyword')
+            for key in qs_keyword:
+                str_key = key.keyword.strip()
+                if file_dict.get(str_key) == "To_translate":
+                    pass
+                elif str_key in file_dict.keys() and str_key not in file_dict.values():
+                    key.keyword = file_dict.get(str_key)
+                    key.save()
+                    count += 1
+                elif str_key in file_dict.values():
+                    logger.info(f"{str_key} has already been translated!")
+                else:
+                    print(untranslated_nb, str_key)
+                    print("___________")
+                    file_dict[str_key] = "To_translate"
+                    untranslated_nb += 1
 
-                logger.info(f"{count} Items have been updated")
-                logger.info(f"{untranslated_nb} Items have been added to the csv file {options['file']}")
-        else:
-            with open(os.path.join(options["file"]), 'w+', encoding='utf-8', newline='') as data_file:
-                data = csv.DictWriter(data_file, fieldnames=['French_keywords', 'English_keywords'], delimiter='\t')
-                data.writeheader()
+            # f = open(options["file"], 'r+')
+            # f.truncate(0)
+            with open(os.path.join(options["file"]), 'w', newline='') as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=['French_keywords', 'English_keywords'], delimiter='\t')
+                writer.writeheader()
+                for key, value in file_dict.items():
+                    writer.writerow({'French_keywords': key, 'English_keywords': value})
 
-                qs_dict = Keyword.objects.all().values().order_by('keyword')
-                i = 0
-                for row in qs_dict:
-                    data.writerow({'French_keywords': str(row['keyword']).strip(), 'English_keywords': 'To_translate'})
-                    i += 1
-                data_file.close()
-            logger.info(f"{i} keywords have been added to the new file named {options['file']}")
+            logger.info(f"{count} Items have been updated")
+            logger.info(f"{untranslated_nb} Items have been added to the csv file {options['file']}")
