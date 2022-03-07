04843545befba58dabbb1081e407a5db1829e26b
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2022-03-07 10:47:17 +0100

    cleanup catalog

--- ifbcat_api/management/commands/cleanup_catalog.py
+++ ifbcat_api/management/commands/cleanup_catalog.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
+from django.db.models import Q
 
 from ifbcat_api import models
 from ifbcat_api.admin import KeywordAdmin
@@ -11,5 +13,13 @@ class Command(BaseCommand):
         ka.split_by_comma_and_remove(None, models.Keyword.objects.all())
         ka.merge_other_where_only_case_differs(None, models.Keyword.objects.all())
         models.Keyword.objects.filter(keyword="").delete()
+
+        # Remove nan of users
+        get_user_model().objects.filter(homepage="nan").update(homepage=None)
+
+        # Default description of teams
+        models.Team.objects.filter(Q(description__isnull=True) | Q(description="")).update(
+            description="To be completed..."
+        )
 
         # End
