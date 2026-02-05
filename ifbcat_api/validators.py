c1377e9e584cfd19c15b2a1dae9a25c82584905d
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2026-02-05 15:16:38 +0100

    update django

--- ifbcat_api/validators.py
+++ ifbcat_api/validators.py
@@ -10,9 +10,9 @@ __p_grid_regexp = '^grid.[0-9]{4,}.[a-f0-9]{1,2}$'
 __p_grid = re.compile(__p_grid_regexp, re.IGNORECASE | re.UNICODE)
 __p_ror_regexp = '^0[0-9a-zA-Z]{6}[0-9]{2}$'
 __p_ror = re.compile(__p_ror_regexp, re.IGNORECASE | re.UNICODE)
-__p_doi_regexp = '^10.\d{4,9}/.+$'
+__p_doi_regexp = r'^10.\d{4,9}/.+$'
 __p_doi = re.compile(__p_doi_regexp, re.IGNORECASE | re.UNICODE)
-__looked_up_regexp = "^[\w \\-_~\\']+$"  # could ba allowed ?;:(),
+__looked_up_regexp = r"^[\w \-_~\']+$"  # could ba allowed ?;:(),
 __looked_up = re.compile(__looked_up_regexp, re.IGNORECASE | re.UNICODE)
 
 
