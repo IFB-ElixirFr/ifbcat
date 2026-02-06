fa236604fc699e0514e5c91749af673fd570dda0
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2026-02-06 19:19:59 +0100

    add example for specific tests

--- ifbcat_api/tests/test_sitemap.py
+++ ifbcat_api/tests/test_sitemap.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 from django.urls import reverse
 
+from ifbcat_api import models
 from ifbcat_api.tests.test_no_views_crash import TestCaseWithData
 
 
@@ -10,6 +11,7 @@ class TestApi(TestCaseWithData):
     by_passed_links = {
         'http://testserver/api/team/Pasteur%20HUB/',
     }
+    link_count_to_test = 10
 
     def test_create_new_file(self):
         response = self.client.get(reverse('sitemap-general'))
@@ -22,9 +24,22 @@ class TestApi(TestCaseWithData):
             group = link.find('/', link.find('api') + 4)
             grouped_links[group].add(link)
 
+        total = 0
         for links in grouped_links.values():
-            links_to_test = list(links)[:10]
+            links_to_test = list(links)[: self.link_count_to_test]
             links_to_test.extend(self.by_passed_links & links)
 
             for link in links_to_test:
                 self.assertEqual(self.client.get(link, follow=True).status_code, 200, link)
+                total += 1
+        self.assertGreater(total, 0)
+
+
+class TestApiCustomInstance(TestApi):
+    link_count_to_test = 1000
+
+    def setUp(self):
+        # don't call super().setUp(), create instance instead, can only be an instance in the sitemap
+        models.Team.objects.get_or_create(
+            name="foo",
+        )
